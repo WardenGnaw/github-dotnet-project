db58fb88ec0bdfb9f09b1d09af6f81a954439e8b
Andrew Wang <waan@microsoft.com>
2018-04-20 18:21:18 -0700

    Pushing sample project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Hello World!");
+                Thread.Sleep(new TimeSpan(0, 0, 10));
+            }
+        }
+    }
+}
